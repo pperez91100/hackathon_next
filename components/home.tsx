de9944f47956b7cc77ae4a8a510cf530975ccbf4
Perez <perezpablopro91@gmail.com>
2024-05-28 23:13:48 +0200

    update

--- components/home.tsx
+++ components/home.tsx
@@ -5,6 +5,10 @@ import { FlipWords } from "@/components/ui/flip-words";
 
 export default function Home() {
 
+  const addLeadingZero = (value: number) => {
+    return value < 10 ? `0${value}` : value;
+  };
+  
   useEffect(() => {
     const targetDate = new Date('2024-07-26T19:30:00');
     const interval = setInterval(() => {
@@ -43,7 +47,7 @@ export default function Home() {
             </p>
             <div className="mt-4 p-4 bg-white bg-opacity-20 rounded-lg shadow-md text-center">
                 <p className="text-3xl font-bold text-white">
-                    {timeLeft.days} : {timeLeft.hours} : {timeLeft.minutes} : {timeLeft.seconds}
+                  {addLeadingZero(timeLeft.days)} : {addLeadingZero(timeLeft.hours)} : {addLeadingZero(timeLeft.minutes)} : {addLeadingZero(timeLeft.seconds)}
                 </p>
                 <p className="text-lg font-bold text-white">
                     Paris 2024 Olympic Games - July 26 to August 11, 2024
@@ -53,7 +57,7 @@ export default function Home() {
         </div>
         <img
           alt="Cover Image"
-          className="w-full max-h-screen object-cover block lg:hidden px-6"
+          className="block lg:hidden px-6"
           src="/img_jo2.png"
         />
         <div className="hidden lg:block relative w-full min-h-screen">
@@ -64,19 +68,129 @@ export default function Home() {
             />
             <div className="absolute bottom-0 w-full h-2/4 bg-gradient-to-t from-white to-transparent"></div>
         </div>
-        <div className="mt-8">
-          <h2 className="text-xl font-semibold">OBJECTIFS DU PROJET</h2>
-          <p className="mt-2">
-            {/* Add your project objectives here */}
-          </p>
-        </div>
+        <section className="grid grid-cols-1 md:grid-cols-2 gap-8 p-4 md:p-8">
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl md:text-3xl font-bold">OBJECTIFS DU PROJET</h2>
+            <p className="text-gray-600 dark:text-gray-400 text-base md:text-lg leading-relaxed">
+              Notre objectif est de développer un modèle prédictif robuste pour anticiper les performances des athlètes lors des Jeux Olympiques, en utilisant des données historiques et des techniques avancées d'analyse et de modélisation.
+              <br/><br/>
+              1. Analyser et créer des visualisations sur un ensemble de données olympiques de 1896 à 2016 pour découvrir quels pays restent au sommet en prolongation, par saison et par sport, ce qu'il faut pour qu'un olympien soit au sommet dans chaque sport, la popularité du sport et la relation entre le nombre de médailles olympiques et le PIB du pays.
+              <br/><br/>
+              2. De plus, à partir de l'ensemble de données olympiques, l'apprentissage automatique a été intégré pour prédire le nombre de médailles que 25 pays obtiendraient aux Jeux olympiques de Tokyo de 2020 avec le modèle de régression linéaire, de régression logistique et de moyenne mobile intégrée auto régressive (ARIMA).
 
-        <div className="mt-8">
-          <h2 className="text-xl font-semibold">ÉQUIPE</h2>
-          <p className="mt-2">
-            {/* Add information about your team here */}
-          </p>
-        </div>
+            </p>
+          </div>
+          <img
+            alt="Notre philosophie"
+            className="rounded-2xl"
+            src="/home_1.jpg"
+          />
+        </section>
+          <section className="grid grid-cols-1 md:grid-cols-2 gap-8 p-4 md:p-8">
+            <div className="grid grid-cols-4 gap-8">
+              <div className="flex flex-col items-center">
+                <img
+                  alt="Equipier_01"
+                  className="rounded-full object-cover shadow-md border border-gray-300"
+                  height="100"
+                  src="/pablo.svg"
+                  width="150"
+                />
+                  <p className="mt-2 text-center text-xl font-semibold text-gray-800">Tanguy MOREL</p>
+                  <p className="mt-1 text-center text-lg font-medium text-gray-600">Chef de Projet</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img
+                  alt="Equipier_02"
+                  className="rounded-full object-cover shadow-md border border-gray-300"
+                  height="100"
+                  src="/pablo.svg"
+                  width="150"
+                />
+                  <p className="mt-2 text-center text-xl font-semibold text-gray-800">Romain RULLIER</p>
+                  <p className="mt-1 text-center text-lg font-medium text-gray-600">Chef de Projet</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img
+                  alt="Equipier_03"
+                  className="rounded-full object-cover shadow-md border border-gray-300"
+                  height="100"
+                  src="/pablo.svg"
+                  width="150"
+                />
+                  <p className="mt-2 text-center text-xl font-semibold text-gray-800">Pablo PEREZ</p>
+                  <p className="mt-1 text-center text-lg font-medium text-gray-600">Chef de Projet</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img
+                  alt="Equipier_04"
+                  className="rounded-full object-cover shadow-md border border-gray-300"
+                  height="100"
+                  src="/pablo.svg"
+                  width="150"
+                />
+                  <p className="mt-2 text-center text-xl font-semibold text-gray-800">Kaotar TEMOUDEN</p>
+                  <p className="mt-1 text-center text-lg font-medium text-gray-600">Chef de Projet</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img
+                  alt="Equipier_05"
+                  className="rounded-full object-cover shadow-md border border-gray-300"
+                  height="100"
+                  src="/pablo.svg"
+                  width="150"
+                />
+                  <p className="mt-2 text-center text-xl font-semibold text-gray-800">Adrien MANFREDI</p>
+                  <p className="mt-1 text-center text-lg font-medium text-gray-600">Chef de Projet</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img
+                  alt="Equipier_06"
+                  className="rounded-full object-cover shadow-md border border-gray-300"
+                  height="100"
+                  src="/pablo.svg"
+                  width="150"
+                />
+                  <p className="mt-2 text-center text-xl font-semibold text-gray-800">Aboubakar MEITE</p>
+                  <p className="mt-1 text-center text-lg font-medium text-gray-600">Chef de Projet</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img
+                  alt="Equipier_07"
+                  className="rounded-full object-cover shadow-md border border-gray-300"
+                  height="100"
+                  src="/pablo.svg"
+                  width="150"
+                />
+                <p className="mt-2 text-center text-xl font-semibold text-gray-800">Douâa ABID</p>
+                <p className="mt-1 text-center text-lg font-medium text-gray-600">Chef de Projet</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <img
+                  alt="Equipier_08"
+                  className="rounded-full object-cover shadow-md border border-gray-300"
+                  height="100"
+                  src="/pablo.svg"
+                  width="150"
+                />
+                  <p className="mt-2 text-center text-xl font-semibold text-gray-800">Nada AL IBRAHIMI</p>
+                  <p className="mt-1 text-center text-lg font-medium text-gray-600">Chef de Projet</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4 order-1 md:order-2">
+              <h2 className="text-2xl md:text-3xl font-bold">NOTRE EQUIPE</h2>
+              <p className="text-gray-600 dark:text-gray-400 text-base md:text-lg leading-relaxed">
+                Notre équipe est composée de professionnels passionnés et talentueux, qui travaillent en étroite
+                collaboration pour vous offrir une expérience exceptionnelle.<br/><br/> 
+                Ensemble, nous repoussons les limites de l'innovation pour vous apporter des solutions sur mesure.
+                <br/><br/> 
+                Guidés par notre passion commune pour les données et l'analyse, nous nous efforçons de créer un modèle prédictif de pointe pour anticiper les performances des athlètes lors des Jeux Olympiques.
+                <br/><br/>
+                Avec notre expertise collective et notre engagement envers l'excellence, nous sommes déterminés à fournir des prédictions précises et fiables qui aideront à éclairer les stratégies et les décisions dans le domaine du sport de haut niveau.
+              </p>
+            </div>
+          </section>
       </div>
   );
 }
