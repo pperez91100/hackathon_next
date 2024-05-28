8d613cb2a9a7308278cc183cd1b1af9cfd941f75
Perez <perezpablopro91@gmail.com>
2024-05-29 01:04:40 +0200

    test prod

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import { BASE_API_URL } from "@/utils/constants"
 import Home from "@/components/home";
 
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-
 export default async function Page() {
 
   const response = await fetch(`${BASE_API_URL}/api/test`);
