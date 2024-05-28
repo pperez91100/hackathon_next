96a318389e69676cd09334730e6c06af3cc09651
Perez <perezpablopro91@gmail.com>
2024-05-28 20:11:51 +0200

    Update home.tsx

--- components/home.tsx
+++ components/home.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
   return (
       <div className="w-full">
-        <div className="absolute top-48 right-32 transform -translate-y-1/2 h-[40rem] lg:flex justify-center items-center px-4 hidden">
+        <div className="absolute top-48 right-32 transform -translate-y-1/2 h-[40rem] lg:flex justify-center items-center px-4 hidden z-50">
           <div className="text-2xl mx-auto font-normal text-white text-center bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 py-6 px-8 rounded-lg shadow-lg">
             <div className="inline-block">
               <div>WELCOME TO THE <FlipWords className="text-white" words={words} /></div>
@@ -56,12 +56,14 @@ export default function Home() {
           className="w-full max-h-screen object-cover block lg:hidden p-6"
           src="/img_jo2.png"
         />
-        <img
-          alt="Cover Image"
-          className="w-full min-h-screen object-cover hidden lg:block"
-          src="/img_jo.png"
-        />
-        
+        <div className="relative w-full min-h-screen">
+            <img
+                alt="Cover Image"
+                className="w-full h-full object-cover hidden lg:block"
+                src="/img_jo.png"
+            />
+            <div className="absolute bottom-0 w-full h-2/4 bg-gradient-to-t from-white to-transparent"></div>
+        </div>
         <div className="mt-8">
           <h2 className="text-xl font-semibold">OBJECTIFS DU PROJET</h2>
           <p className="mt-2">
