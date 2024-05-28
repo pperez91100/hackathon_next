44898fb5a754718296ce5a767e2904df9c6150e3
Perez <perezpablopro91@gmail.com>
2024-05-28 17:03:52 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const fontSans = FontSans({
  
 export const metadata: Metadata = {
   title: "Predict JO",
-  description: "Predictions for the Olympic Games",
+  description: "Predictions for the Olympic Games 2024",
 };
 
 export default async function RootLayout({
