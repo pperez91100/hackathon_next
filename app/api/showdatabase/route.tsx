8ecb7df840af24ae218aa738af49e11b57a2f985
Perez <perezpablopro91@gmail.com>
2024-05-29 23:05:43 +0200

    test prod

--- app/api/showdatabase/route.tsx
+++ app/api/showdatabase/route.tsx
@@ -2,29 +2,7 @@ import { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import pool from '@/app/db';
 
-function formatColumnName(columnName: string): string {
-    return columnName
-        .split('_')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(' ');
-}
 
-function formatRows(rows: any[]): any[] {
-    return rows.map(row => {
-        const formattedRow: any = {};
-        for (const key in row) {
-            if (row.hasOwnProperty(key)) {
-                let value = row[key];
-                // Remplacer les valeurs vides par 'N'
-                if (value === null || value === undefined || value === '') {
-                    value = 'NULL';
-                }
-                formattedRow[formatColumnName(key)] = value;
-            }
-        }
-        return formattedRow;
-    });
-}
 
 
 export async function GET(req: NextRequest, res: NextResponse) {
@@ -32,21 +10,16 @@ export async function GET(req: NextRequest, res: NextResponse) {
         // Sélectionner toutes les lignes de la table 'athletes_jo'
         const athletesJoResult = await pool.query('SELECT * FROM athletes_jo;');
         // Formater les clés des colonnes
-        const athletes_jo = formatRows(athletesJoResult.rows);
+        const athletes_jo = athletesJoResult.rows;
 
         // Sélectionner toutes les lignes de la table 'results_jo'
         const resultsJoResult = await pool.query('SELECT * FROM results_jo;');
         // Formater les clés des colonnes
-        const results_jo = formatRows(resultsJoResult.rows);
+        const results_jo = resultsJoResult.rows;
 
-        return new Response(
-            JSON.stringify({ athletes_jo, results_jo }),
-            {
-                headers: { "Content-Type": "application/json" },
-            }
-        );
+        return NextResponse.json({ athletes_jo, results_jo }, { status: 200 });
     } catch (error) {
         console.error('Error processing request:', error);
-        return new Response('Error processing request.');
+        return new NextResponse('Error processing request.', { status: 500 });
     }
 }
--- app/database/page.tsx
+++ app/database/page.tsx
@@ -3,6 +3,30 @@ import Database from "@/components/database";
 
 export default async function PageDatabase() {
 
+    function formatColumnName(columnName: string): string {
+        return columnName
+            .split('_')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(' ');
+    }
+    
+    function formatRows(rows: any[]): any[] {
+        return rows.map(row => {
+            const formattedRow: any = {};
+            for (const key in row) {
+                if (row.hasOwnProperty(key)) {
+                    let value = row[key];
+                    // Remplacer les valeurs vides par 'N'
+                    if (value === null || value === undefined || value === '') {
+                        value = 'NULL';
+                    }
+                    formattedRow[formatColumnName(key)] = value;
+                }
+            }
+            return formattedRow;
+        });
+    }
+
     const response = await fetch(`${BASE_API_URL}/api/showdatabase`,{ cache: 'no-store' });
     if (!response.ok) {
         throw new Error('Erreur lors de la récupération des données');
@@ -10,7 +34,10 @@ export default async function PageDatabase() {
 
     const data = await response.json();
     
+    const data1 = formatRows(data.athletes_jo);
+    const data2 = formatRows(data.results_jo);
+
     return (
-        <Database data1={data.athletes_jo} data2={data.results_jo} />
+        <Database data1={data1} data2={data2} />
     );
   }
