583b0a542194d6e222b6d5ef68f0256396cd5124
Perez <perezpablopro91@gmail.com>
2024-05-30 14:32:55 +0200

    Update predict.tsx

--- components/predict.tsx
+++ components/predict.tsx
@@ -4,8 +4,7 @@ const Predict: React.FC = () => {
 
     return (
         <div className="flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold text-gray-800">Predict JO</h1>
-            <p className="mt-2 text-center text-lg font-medium text-gray-600">Predict the next Olympic Games</p>
+            <img src="/imghere" ></img>
         </div>
 
     );
