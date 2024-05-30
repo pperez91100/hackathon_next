fd4d739305e286e6064020857f573870bb0d17dc
AdrienManfredi <amimanfredi@gmail.com>
2024-05-30 16:03:12 +0200

    predictions page

--- components/predict.tsx
+++ components/predict.tsx
@@ -4,7 +4,12 @@ const Predict: React.FC = () => {
 
     return (
         <div className="flex flex-col items-center justify-center">
-            <img src="/imghere" ></img>
+            <img src="/total_medailles_IA.png" alt="Médailles IA" />
+            <img src="/gold_medailles_IA.png" alt=" Gold Médailles IA" />
+            <img src="/silver_medailles_IA.png" alt=" Silver Médailles IA" />
+            <img src="/bronze_medailles_IA.png" alt=" Bronze Médailles IA" />
+            <img src="/France_medailles_IA.png" alt=" Gold Médailles IA" />
+
         </div>
 
     );
