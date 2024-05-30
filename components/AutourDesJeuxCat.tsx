5dd1a71956a23b69ce4481d1d78550bbd9a2dc3c
Tanguy <tanguymorel.pro@gmail.com>
2024-05-30 11:41:39 +0200

    style: ajout de style

--- components/AutourDesJeuxCat.tsx
+++ components/AutourDesJeuxCat.tsx
@@ -9,10 +9,14 @@ interface FeaturedVideoProps {
 const AutourDesJeuxCat: React.FC<FeaturedVideoProps> = ({ name, src, url }) => {
 
     return (
-        <a href={url} className=" flex flex-col p-2 border-solid border border-slate-500 rounded-lg hover:-translate-y-1 transition-transform">
+        <a href={url} className="flex flex-col p-2 border-solid border shadow-custom border-slate-200 rounded-lg hover:-translate-y-1 transition-transform duration-300 group">
             <img src={src} width={250} height={250} alt="" />
-            <h3 className="mt-2 ">{name}</h3>
+            <div className="flex justify-between items-end w-full  p-2">
+                <h3 className="mt-2 font-semibold">{name}</h3>
+                <p className="-rotate-90 -translate-x-5 transition-all opacity-0 duration-300 group-hover:opacity-100 group-hover:rotate-0 group-hover:translate-x-0">→</p>
+            </div>
         </a>
+
     );
 }
 
