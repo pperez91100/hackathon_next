c596b6b96e9fc78a97752a61e59e3ae3fa2d969e
Aboubakar <smeite20@gmail.com>
2024-05-30 16:57:46 +0200

    integration graphs clustering

--- components/predict.tsx
+++ components/predict.tsx
@@ -4,7 +4,6 @@ const Predict: React.FC = () => {
 
     return (
         <div className="flex flex-col items-center justify-center">
-<<<<<<< HEAD
             <h2> Methode Coude </h2>
             <img src="/methodeCoude.html" ></img>
             <h2>Clusters ( nbre de medaills par type )</h2>
@@ -13,14 +12,12 @@ const Predict: React.FC = () => {
             <img src="/correltion_avec_pca.html" ></img>
             <h2> Caracteristiques des clusters </h2>
             <img src="/clusters_boc_plots.html" ></img>
-=======
             <img src="/total_medailles_IA.png" alt="Médailles IA" />
             <img src="/gold_medailles_IA.png" alt=" Gold Médailles IA" />
             <img src="/silver_medailles_IA.png" alt=" Silver Médailles IA" />
             <img src="/bronze_medailles_IA.png" alt=" Bronze Médailles IA" />
             <img src="/France_medailles_IA.png" alt=" Gold Médailles IA" />
 
->>>>>>> c1e89735f3aebade32b76ff3ea34d91398632e53
         </div>
 
     );
